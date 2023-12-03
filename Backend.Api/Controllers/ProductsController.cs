8bfccebc5693196598b64dd21526bed221f8b05c
Kamil Szewczyk <szewdev@hotmail.com>
2023-12-03 14:29:41 +0100

    feat(api): Add Product CRUD endpoints

--- Backend.Api/Controllers/ProductsController.cs
+++ Backend.Api/Controllers/ProductsController.cs
@@ -1,10 +1,57 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Backend.Application.Commands;
+using Backend.Application.Commands.Interfaces;
+using Backend.Application.DTOs;
+using Backend.Application.Queries;
+using Backend.Application.Queries.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
-namespace Backend.Api.Controllers
+namespace Backend.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController(IProductCommandHandler commandHandler, IProductQueryHandler queryHandler) : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ProductsController : ControllerBase
+    private readonly IProductCommandHandler _commandHandler = commandHandler;
+    private readonly IProductQueryHandler _queryHandler = queryHandler;
+
+    // POST api/products
+    [HttpPost]
+    public async Task<ActionResult> CreateProduct([FromBody] CreateProductCommand command)
     {
+        if (!ModelState.IsValid) return BadRequest(ModelState);
+
+        var productId = await _commandHandler.Handle(command);
+        return CreatedAtAction(nameof(GetProduct), new { id = productId }, command);
+    }
+
+    // PUT api/products/{id}
+    [HttpPut("{id}")]
+    public async Task<ActionResult> UpdateProduct(int id, [FromBody] UpdateProductCommand command)
+    {
+        if (!ModelState.IsValid) return BadRequest(ModelState);
+
+        await _commandHandler.Handle(command with { Id = id });
+        return NoContent();
+    }
+
+    // GET api/products/{id}
+    [HttpGet("{id}")]
+    public async Task<ActionResult<ProductDto>> GetProduct(int id)
+    {
+        var query = new GetProductQuery(id);
+        var product = await _queryHandler.Handle(query);
+
+        if (product == null) return NotFound();
+
+        return product;
+    }
+
+    // GET api/products}
+    [HttpGet]
+    public async Task<ActionResult<List<ProductDto>>> GetProducts()
+    {
+        var result = await _queryHandler.Handle();
+
+        return result;
     }
 }
--- Backend.Api/Program.cs
+++ Backend.Api/Program.cs
@@ -1,4 +1,18 @@
+using Backend.Application.Commands.Handlers;
+using Backend.Application.Commands.Interfaces;
+using Backend.Application.Queries.Handlers;
+using Backend.Application.Queries.Interfaces;
+using Backend.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IProductCommandHandler, ProductCommandHandler>();
+builder.Services.AddScoped<IProductQueryHandler, ProductQueryHandler>();
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -6,6 +20,9 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseRouting();
+app.UseEndpoints(endpoints => { _ = endpoints.MapControllers(); });
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -16,29 +33,4 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
