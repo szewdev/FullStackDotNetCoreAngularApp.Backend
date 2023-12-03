64a44b30523064dfc4543ffeeca504b5f2706500
Kamil Szewczyk <szewdev@hotmail.com>
2023-12-03 12:24:52 +0100

    feat(domain): Add Base and Product entities

---
+++ Backend.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,30 @@
+﻿namespace Backend.Domain.Entities;
+
+public abstract class BaseEntity
+{
+    protected BaseEntity()
+    {
+        UniqueId = Guid.NewGuid();
+        CreatedAt = DateTimeOffset.Now;
+    }
+
+    public int Id { get; }
+    public Guid UniqueId { get; private set; }
+    public DateTimeOffset CreatedAt { get; private set; }
+    public DateTimeOffset? UpdatedAt { get; private set; }
+    public bool IsDeleted { get; private set; }
+    public bool IsActive => !IsDeleted;
+    public bool IsModified => UpdatedAt != null;
+
+    public void Delete()
+    {
+        IsDeleted = true;
+    }
+
+    public void Restore()
+    {
+        IsDeleted = false;
+    }
+
+}
+
--- Backend.Domain/Entities/Product.cs
+++ Backend.Domain/Entities/Product.cs
@@ -1,7 +1,13 @@
-﻿namespace Backend.Domain.Entities
-{
-    public class Product
-    {
+﻿namespace Backend.Domain.Entities;
 
+public class Product(string name, decimal price) : BaseEntity
+{
+    public string Name { get; private set; } = name;
+    public decimal Price { get; private set; } = price;
+
+    public void UpdateProduct(string name, decimal price)
+    {
+        Name = name;
+        Price = price;
     }
 }
