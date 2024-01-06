deeeb4d5b1a937aa0f47e3d80a8a8ca53225057a
Kamil Szewczyk <szewdev@hotmail.com>
2024-01-06 13:11:23 +0100

    feat(query,dto): Is product active

--- Backend.Application/DTOs/ProductDto.cs
+++ Backend.Application/DTOs/ProductDto.cs
@@ -1,3 +1,3 @@
 ﻿namespace Backend.Application.DTOs;
 
-public record ProductDto(int Id, string Name, decimal Price, string? Description);
+public record ProductDto(int Id, string Name, decimal Price, string? Description, bool IsActive);
